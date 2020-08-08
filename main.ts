65c18e96a37dc0a91f3282adbe068c764840a552
Beedlebub <beedlebub@gmail.com>
2020-08-08 12:18:22 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,26 +5,26 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . . . . . . . . . 
+    . . . . . f f f f . . . . . . . . . . . . . . . 
+    . . . f f f 2 2 f f f . . . . . . . . . . . . . 
+    . . f f f 2 2 2 2 f f f . . . . . . . . . . . . 
+    . f f f e e e e e e f f f . . . . . . . . . . . 
+    . f f e 2 2 2 2 2 2 e e f . . . . . . . . . . . 
+    . f e 2 f f f f f f 2 e f . . . . . . . . . . . 
+    . f f f f e e e e f f f f . . . . . . . . . . . 
+    f f e f b f 4 4 f b f e f f . . . . . . . . . . 
+    f e e 4 1 f d d f 1 4 e e f . . . . . . . . . . 
+    . f f f f d d d d d e e f . . . . . . . . . . . 
+    f d d d d f 4 4 4 e e f . . . . . . . . . . . . 
+    f b b b b f 2 2 2 2 f 4 e . . . . . . . . . . . 
+    f b b b b f 2 2 2 2 f d 4 . . . . . . . . . . . 
+    . f c c f 4 5 5 4 4 f 4 4 . . . . . . . . . . . 
+    . . f f f f f f f f . . . . . . . . . . . . . . 
+    . . . . f f . . f f . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . f f f f . . . . . . . . . . 
-    . . . . . . . . f f 1 1 1 1 f f . . . . . . . . 
-    . . . . . . . f b 1 1 1 1 1 1 b f . . . . . . . 
-    . . . . . . . f 1 1 1 1 1 1 1 1 f . . . . . . . 
-    . . . . . . f d 1 1 1 1 1 1 1 1 d f . . . . . . 
-    . . . . 7 . f d 1 1 1 1 1 1 1 1 d f . . . . . . 
-    . . . 7 . . f d 1 1 1 1 1 1 1 1 d f . . . . . . 
-    . . . 7 . . f d 1 1 1 1 1 1 1 1 d f . . . . . . 
-    . . . 7 . . f d d d 1 1 1 1 d d d f f . . . . . 
-    . . . 7 7 . f b d b f d d f b d b f c f . . . . 
-    . . . 7 7 7 f c d c f 1 1 f c d c f b f . . . . 
-    . . . . 7 7 f f f b d b 1 b d f f c f . . . . . 
-    . . . . f c b 1 b c f f f f f f . . . . . . . . 
-    . . . . f 1 c 1 c 1 f f f f f f . . . . . . . . 
-    . . . . f d f d f d f f f f f . . . . . . . . . 
-    . . . . . f . f . f . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . . . . . . . . . 
